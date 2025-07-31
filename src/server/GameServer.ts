a2210a6a8898ad7be5e832d92d23401d75aa0726
1brucben <1benjbruce@gmail.com>
2025-07-31 23:05:19 +0200

    Call remove all listeners on ws close (#1558)
    After doing a memory dumb, there is a websocket handler that hadn't been
    cleaned up. So ensure removeAllListeners is called.
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I have read and accepted the CLA aggreement (only required once).
    regression is found:
    
    evan

--- src/server/GameServer.ts
+++ src/server/GameServer.ts
@@ -203,6 +203,7 @@ export class GameServer {
               } satisfies ServerErrorMessage),
             );
             client.ws.close(1002, "ClientMessageSchema");
+            client.ws.removeAllListeners();
             return;
           }
           const clientMsg = parsed.data;
@@ -260,6 +261,7 @@ export class GameServer {
     });
     client.ws.on("error", (error: Error) => {
       if ((error as any).code === "WS_ERR_UNEXPECTED_RSV_1") {
+        client.ws.removeAllListeners();
         client.ws.close(1002, "WS_ERR_UNEXPECTED_RSV_1");
       }
     });
@@ -460,6 +462,7 @@ export class GameServer {
         });
         if (client.ws.readyState === WebSocket.OPEN) {
           client.ws.close(1000, "no heartbeats received, closing connection");
+          client.ws.removeAllListeners();
         }
       } else {
         alive.push(client);
