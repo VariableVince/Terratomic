3303b0f9241ed60ae36d51a6655b40cbf3510f83
1brucben <1benjbruce@gmail.com>
2025-07-31 19:59:36 +0200

    Extend winner schema (#1333)
    Extend the winner schema to support multi-player wins (vote for peace,
    teams).
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I understand that submitting code with bugs that could have been
    caught through manual testing blocks releases and new features for all
    contributors

--- src/client/ClientGameRunner.ts
+++ src/client/ClientGameRunner.ts
@@ -7,7 +7,6 @@ import {
   GameStartInfo,
   PlayerRecord,
   ServerMessage,
-  Winner,
 } from "../core/Schemas";
 import { createGameRecord } from "../core/Util";
 import { ServerConfig } from "../core/configuration/Config";
@@ -201,13 +200,6 @@ export class ClientGameRunner {
     this.lastMessageTime = Date.now();
   }
 
-  private getWinner(update: WinUpdate): Winner {
-    if (update.winner[0] !== "player") return update.winner;
-    const clientId = this.gameView.playerBySmallID(update.winner[1]).clientID();
-    if (clientId === null) return;
-    return ["player", clientId];
-  }
-
   private saveGame(update: WinUpdate) {
     if (this.myPlayer === null) {
       return;
@@ -220,7 +212,6 @@ export class ClientGameRunner {
         stats: update.allPlayersStats[this.lobby.clientID],
       },
     ];
-    const winner = this.getWinner(update);
 
     if (this.lobby.gameStartInfo === undefined) {
       throw new Error("missing gameStartInfo");
@@ -233,7 +224,7 @@ export class ClientGameRunner {
       [],
       startTime(),
       Date.now(),
-      winner,
+      update.winner,
     );
     endGame(record);
   }
