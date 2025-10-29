ddde46783a03d1fa5ef0958c9b9dc00caeb8bd7c
1brucben <1benjbruce@gmail.com>
2025-10-29 14:31:44 +0100

    feat: add peace timer duration configuration to game settings

--- src/server/GameServer.ts
+++ src/server/GameServer.ts
@@ -110,6 +110,10 @@ export class GameServer {
     if (gameConfig.playerTeams !== undefined) {
       this.gameConfig.playerTeams = gameConfig.playerTeams;
     }
+    if (gameConfig.peaceTimerDurationMinutes !== undefined) {
+      this.gameConfig.peaceTimerDurationMinutes =
+        gameConfig.peaceTimerDurationMinutes;
+    }
   }
 
   public addClient(client: Client, lastTurn: number) {
