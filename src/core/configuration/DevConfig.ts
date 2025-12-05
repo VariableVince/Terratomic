39726840bb064c88ed75d4ffa74b192942e05e9d
1brucben <1benjbruce@gmail.com>
2025-12-05 13:18:42 +0100

    fix: correct game creation rate in DevServerConfig

--- src/core/configuration/DevConfig.ts
+++ src/core/configuration/DevConfig.ts
@@ -14,7 +14,7 @@ export class DevServerConfig extends DefaultServerConfig {
   }
 
   gameCreationRate(): number {
-    return 180 * 1000;
+    return 3 * 1000;
   }
 
   samWarheadHittingChance(): number {
